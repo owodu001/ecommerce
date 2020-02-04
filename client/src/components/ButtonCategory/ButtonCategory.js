0c6b144352b45f11f376e44a76a91b9bc51816ca
Pat Brown <ptrckbrown94@gmail.com>
2020-02-04 09:52:26 -0600

    navbar and catergory button

---
+++ client/src/components/ButtonCategory/ButtonCategory.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Button } from "grommet";
+
+
+function ButtonCatergory () {
+
+    return (
+        <Button
+        label="Category"
+        onClick={() => {}}
+      />
+
+    );
+}
+
+export default ButtonCatergory;
