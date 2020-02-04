8422f4633c261c011ee6799eefccc84d71a7a3d1
Paul Chheang <phalcheat@gmail.com>
2020-02-04 11:51:34 -0600

    set up route in App.js

---
+++ client/src/components/layout/Nav.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function () {
+
+    const styles = {
+        nav: {
+            backgroundColor: "black",
+            color: "white",
+
+        },
+        navLink: {
+            listStyle: "none",
+            padding: "30px"
+        },
+        li: {
+            display: "inline",
+            textDecoration: "none",
+            paddingRight: "15px"
+        },
+        link: {
+            color: "white"
+        }
+
+    }
+
+    return (
+        <nav style={styles.nav}>
+            <ul style={styles.navLink}>
+                <Link to="/" style={styles.link}> <li style={styles.li}> Home</li></Link>
+                <Link to="/product" style={styles.link}> <li style={styles.li}> Product Detail</li></Link>
+                <Link to="/sign-in" style={styles.link}> <li style={styles.li}> Sign In</li></Link>
+                <Link to="/cart" style={styles.link}> <li style={styles.li}> Cart</li></Link>
+            </ul>
+        </nav>
+
+
+    );
+}
