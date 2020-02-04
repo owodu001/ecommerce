61be2dc1c597413eab527e346081482525eefb77
Chelley <michelle.owodunni@gmail.com>
2020-02-04 12:10:45 -0600

    product detail page layout

--- client/src/components/product/Product.js
+++ client/src/components/product/Product.js
@@ -1,73 +1,64 @@
 import React from "react";
 import {
-  Box,
-  Grid,
-  Tab,
-  Tabs,
-  Paragraph,
+  Card,
   Button,
+  Container,
+  Col,
+  Row,
   Image,
-  Anchor,
-  Nav
-} from "grommet";
+  InputGroup,
+  FormControl
+} from "react-bootstrap";
 
 // import { Link } from "react-router-dom";
 
 export default function() {
   return (
     <>
-      {/* <Box
-        justify="center"
-        align="center"
-        style={{ marginTop: "4rem" }}
-        height="small"
-        width="small"
-      >
-        
-      </Box> */}
-
-      <Box justify="center" align="center" style={{ marginTop: "4rem" }}>
-        <Image
-          fit="cover"
-          src="//v2.grommet.io/assets/Wilderpeople_Ricky.jpg"
-        />
-        <Paragraph>
-          {`You know, sometimes in life it seems like there's no way out. Like
-      a sheep trapped in a maze designed by wolves.`}
-        </Paragraph>
-
-        <Button label="Add to Cart" onClick={() => {}} />
-      </Box>
-
-      {/* <Nav direction="row" background="brand" pad="medium">
-        <Anchor icon={<Icons.Home />} hoverIndicator />
-        <Anchor icon={<Icons.Notification />} hoverIndicator />
-        <Anchor icon={<Icons.ChatOption />} hoverIndicator />
-      </Nav> */}
-
-      <Grid
-        rows={["xxsmall", "xsmall"]}
-        columns={["xsmall", "small"]}
-        gap="small"
-        areas={[
-          { name: "header", start: [0, 0], end: [1, 0] },
-          { name: "nav", start: [0, 1], end: [0, 1] },
-          { name: "main", start: [1, 1], end: [1, 1] }
-        ]}
-      >
-        <Box gridArea="header" background="brand" />
-        <Box gridArea="nav" background="light-5" />
-        <Box gridArea="main" background="light-2" />
-      </Grid>
-
-      <Tabs>
-        <Tab title="tab 1">
-          <Box pad="medium">One</Box>
-        </Tab>
-        <Tab title="tab 2">
-          <Box pad="medium">Two</Box>
-        </Tab>
-      </Tabs>
+      <Container>
+        <Row className="mt-5">
+          <Col md={6}>
+            <Image src="http://placekitten.com/400/400" rounded />
+          </Col>
+          <Col md={6}>
+            <Row>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" src="http://placekitten.com/400/400" />
+                <Card.Body>
+                  <Card.Title>
+                    WD·NY Black – Men's Palm Burgundy Jacket
+                  </Card.Title>
+                  <Card.Title>$760</Card.Title>
+                  <Card.Text>
+                    Bring some funky nostalgia to your next event — and every
+                    one after — with the Denim Blazer WD·NY Black – Men's Palm
+                    Burgundy Jacket. This suit jacket is the perfect way to add
+                    some fun style to your dressy look. Pair it with a floral
+                    button-down shirt and dress pants for the perfect mix of
+                    formal and unique.
+                  </Card.Text>
+                  <InputGroup className="mb-3">
+                    <InputGroup.Prepend>
+                      <Button variant="info">-</Button>{" "}
+                    </InputGroup.Prepend>
+                    <FormControl
+                      type="text"
+                      placeholder="Input group example"
+                      aria-label="Input group example"
+                      aria-describedby="btnGroupAddon"
+                    />
+                    <InputGroup.Append>
+                      <Button variant="info">+</Button>{" "}
+                    </InputGroup.Append>
+                  </InputGroup>
+                  <Button variant="info">Add to Cart</Button>
+                </Card.Body>
+              </Card>
+            </Row>
+            <Row>Product Details</Row>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
