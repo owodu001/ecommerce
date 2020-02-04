cc56ee4084bf860d935cce7d25a9f2cf337c3285
Pat Brown <ptrckbrown94@gmail.com>
2020-02-04 12:24:41 -0600

    updated Navbars

--- client/src/components/NavMain/Navbar.js
+++ client/src/components/NavMain/Navbar.js
@@ -4,23 +4,28 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Nav from "react-bootstrap/Nav";
+import { Link } from 'react-router-dom';
 
 
 function Navbar() {
 
-    return (
-        <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Our Store</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#signin">Sign in</Nav.Link>
-          <Nav.Link href="#cart">Cart</Nav.Link>
-        </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-info">Search</Button>
-        </Form>
-      </Navbar>
-    );
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home">Our Store</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link>
+          <Link to="/home">Sign In</Link>
+        </Nav.Link>
+        <Nav.Link>
+          <Link to="/home">Cart</Link>
+        </Nav.Link>
+      </Nav>
+      <Form inline>
+        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        <Button variant="outline-info">Search</Button>
+      </Form>
+    </Navbar>
+  );
 }
 export default Navbar;
