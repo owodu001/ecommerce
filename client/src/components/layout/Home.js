ec9a24bccb69f0029746b91464ed38d41aa4a070
Chelley <michelle.owodunni@gmail.com>
2020-02-05 12:25:01 -0600

    import data

--- client/src/components/layout/Home.js
+++ client/src/components/layout/Home.js
@@ -1,6 +1,7 @@
-import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "./Home.css"
+import React, { useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Home.css";
+import axios from "axios";
 
 import HeroBanner from "../HeroBanner/HeroBanner";
 import ProductGrid from "../ProductGrid/ProductGrid";
@@ -10,30 +11,25 @@ import CategoryBar from "../categoryBar/CategoryBar";
 import FootNav from "../Footer/Footer";
 import Pages from "../Pagination/Pagination";
 
+export default function() {
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/products")
+      .then(res => console.log(res.data));
+  }, []);
 
-
-export default function () {
-
-    // useEffect(() => {
-    //     axios
-    //         .get("http://localhost:5000/api/products")
-    //         .then(res => console.log(res.data));
-    // }, []);
-
-    return (
-        <>
-            <HeroBanner className="hero" spacing="base" />
-            <CategoryBar />
-            <hr />
-            <SearchComp />
-            <hr />
-            <ProductGrid className="grid" spacing="base" />
-            <hr />
-            <Pages />
-            <hr />
-            <FootNav />
-
-        </>
-    );
+  return (
+    <>
+      <HeroBanner className="hero" spacing="base" />
+      <CategoryBar />
+      <hr />
+      <SearchComp />
+      <hr />
+      <ProductGrid className="grid" spacing="base" />
+      <hr />
+      <Pages />
+      <hr />
+      <FootNav />
+    </>
+  );
 }
-
