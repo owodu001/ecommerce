8529e5f7222bc5c8fc6572031349b5ce44941dbf
Mary Jacobs <TheMaryJacobs@gmail.com>
2020-02-05 11:16:40 -0600

    build cart item component

--- client/src/components/layout/Cart.js
+++ client/src/components/layout/Cart.js
@@ -1,8 +1,21 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import CartProduct from "../Cart/CartProduct";
+import { Col, Row, Container} from "react-bootstrap";
+import CartSubtotal from "../Cart/CartSubtotal";
+
 
 export default function () {
     return (
-        <h1>Cart Page</h1>
+    <Container>
+            <Row>
+            <Col>
+            <CartProduct/>
+            </Col>
+            <Col>
+            {/* <CardSubtotal/> */}
+            </Col>
+            </Row>
+    </Container>
     );
-}
+};
