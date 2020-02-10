05a60c6289d3d38e6f4ddff329bd0c2a497113d8
Pat Brown <ptrckbrown94@gmail.com>
2020-02-10 14:54:18 -0600

    updated readme.md

--- client/src/components/PayPalButton/PayPalButton.js
+++ client/src/components/PayPalButton/PayPalButton.js
@@ -8,15 +8,17 @@ export default function PayButton({cartSubtotal}) {
       // shippingPreference="NO_SHIPPING" // default is "GET_FROM_FILE"
       onSuccess={(details, data) => {
         alert("Transaction completed by " + details.payer.name.given_name);
-
+      
         // OPTIONAL: Call your server to save the transaction
         return fetch("/paypal-transaction-complete", {
           method: "post",
           body: JSON.stringify({
             orderId: data.orderID
           })
-        });
+        });        
+
       }}
+      
       options={{
         // The client ID needs to be the business ID from PayPal
         // This is needed to make sure the business gets the correct amount
