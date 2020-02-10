facde3886a01e4a14865bfed5f7910cdeb319205
Mary Jacobs <TheMaryJacobs@gmail.com>
2020-02-09 19:55:23 -0600

    style search sort component make mobile responsive

--- client/src/components/Search/SearchComp.js
+++ client/src/components/Search/SearchComp.js
@@ -16,10 +16,10 @@ const SearchComp = ({ searchProducts }) => {
   return (
     <Container className="SearchComp">
       <Row className="justify-content-center">
-        <Col className="column" sm={3}>
+        <Col className="column" lg={6} sm={3}>
           <Dropdown>
             <Dropdown.Toggle
-              className="dropd"
+              className="searchBtn"
               variant="info"
               id="dropdown-basic"
             >
@@ -35,7 +35,7 @@ const SearchComp = ({ searchProducts }) => {
             </Dropdown.Menu>
           </Dropdown>
         </Col>
-        <Col className="column" sm={3}>
+        {/* <Col className="column" sm={3}>
           <Dropdown>
             <Dropdown.Toggle
               className="dropd"
@@ -43,22 +43,21 @@ const SearchComp = ({ searchProducts }) => {
               id="dropdown-basic"
             >
               Filter by:
-            </Dropdown.Toggle>
+            </Dropdown.Toggle> */}
             {/* UPDATE THIS DROPDOWN MENU TO FILTER BY YOUR SPECIFIC CATEGORIES */}
-            <Dropdown.Menu>
+            {/* <Dropdown.Menu>
               <Dropdown.Item href="#/action-1">Category 1</Dropdown.Item>
               <Dropdown.Item href="#/action-2">Category 2</Dropdown.Item>
               <Dropdown.Item href="#/action-2">Category 3</Dropdown.Item>
               <Dropdown.Item href="#/action-2">Category 4</Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
-        </Col>
+        </Col> */}
         <Col className="column" sm={6}>
           <InputGroup className="mb-3">
             <InputGroup.Prepend>
               <Button
-                className="dropd"
-                variant="outline-secondary"
+                className="searchBtn"
                 onClick={() => {
                   console.log(value);
                   searchProducts(value);
@@ -68,6 +67,7 @@ const SearchComp = ({ searchProducts }) => {
               </Button>
             </InputGroup.Prepend>
             <FormControl
+              className="searchInput"
               aria-describedby="basic-addon1"
               value={value}
               onChange={e => setValue(e.target.value)}
